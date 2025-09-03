2ad7c8428869e62cc70ca7e2cedc8e6159f33bae
A-bacate <giovanna.c.magagnin@gmail.com>
2025-09-03 10:38:35 -0300

    prog

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+// Importar bibliotecas
 const express = require("express")
 const sqlite3 = require('sqlite3')
 const cors = require('cors')
 const bcrypt = require('bcrypt')
 
+// Configurar servidor
 const app = express();
 const PORT = 3000;
 
@@ -11,3 +13,10 @@ app.use(express.json())
 
 // Criar banco sqlite e tabela
 const db = new sqlite3.Database('./database.db')
+// Criar tabela usuarios
+db.run(`CREATE TABLE IF NOT EXISTS usuarios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT,
+    email TEXT,
+    senha TEXT
+)`)
