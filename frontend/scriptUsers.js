68bb2de072fbae3786af99c6a8692a3b9dc8cd2a
Abacate <giovanna.c.magagnin@gmail.com>
2025-09-24 09:40:39 -0300

    programação

--- frontend/scriptUsers.js
+++ frontend/scriptUsers.js
@@ -3,6 +3,22 @@ function carregar(){
     fetch('http://localhost:3000/usuarios') // buscou no servidor
     .then(res => res.json()) // transformei a resposta em json (só pra garantir)
     .then(data => { // array com todos os dados que foram buscados (o professor gosta de chamar de 'data' porque são nossos dados :D )
+
         const tbody = document.getElementById('tabela')
+        // limpar dados da tabela para ela não ficar repetindo toda vez que o site é carregado
+        tbody.innerHTML = ""
+
+        console.log(data);
+
+        // tristeza e dor e sofrimento
+        data.forEach(user => {
+            const td = document.createElement('tr')
+            true.innerHTML = `
+                <td>${user.id}</td>
+                <td>${user.nome}</td>
+                <td>${user.email}</td>
+                <td></td>
+            `
+        });
     })
 }
